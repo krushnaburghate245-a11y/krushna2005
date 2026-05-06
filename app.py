207794567457bea379363b73bb97c16c86eda1bd
krushnaburghate245-a11y <krushnaburghate245@gmail.com>
2026-05-07 01:12:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
 from flask import Flask
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/')
 
 def home():
     return "Hello cloud"
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
